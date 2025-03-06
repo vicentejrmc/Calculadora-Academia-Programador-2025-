dfda694a70abd4eec5526609a3afdf9fd6abe1bb
vicentejrmc <vicente_pc@outlook.com>
2025-03-06 17:31:16 -0300

    Adionada opção de subtração

--- Calculadora.ConsoleApp/Program.cs
+++ Calculadora.ConsoleApp/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("__________________________");
             Console.WriteLine("Calculadora Tabajara 2025");
+            Console.WriteLine("__________________________");
+
+            Console.WriteLine(" 1 - Somar");
+            Console.WriteLine(" 2 - Subtrair");
+            Console.WriteLine(" S - Sair");
+
+            Console.WriteLine();
+            Console.Write("Escolha uma opção: ");
+            string opcao = Console.ReadLine().ToUpper(); // Converte para Maiúsculo         
+            
+            if(opcao == "S")
+            {
+                return;
+            }
 
             Console.Write("Digite o primeiro numero: ");
             string primeiroNumeroString = Console.ReadLine();
@@ -13,8 +28,17 @@
             Console.Write("Digite o segundo numero: ");
             string segundoNumeroString = Console.ReadLine();
             int segundoNumero = Convert.ToInt32(segundoNumeroString);
+            
+            int resultado = 0;
 
-            int resultado = primeiroNumero + segundoNumero;
+            if (opcao == "1")
+            {
+                resultado = primeiroNumero + segundoNumero;
+            }
+            else
+            {
+                resultado = primeiroNumero - segundoNumero;
+            }
 
             Console.WriteLine("Resultado: " + resultado);
 
