b3b0b2a91f421f049299b897fff2fcc3a1e7fa7d
vicentejrmc <vicente_pc@outlook.com>
2025-03-06 17:05:54 -0300

    Add project files.

---
+++ Calculadora.ConsoleApp/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Calculadora.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculadora Tabajara 2025");
+
+            Console.Write("Digite o primeiro numero: ");
+            string primeiroNumeroString = Console.ReadLine();
+            int primeiroNumero = Convert.ToInt32(primeiroNumeroString); 
+                
+            Console.Write("Digite o segundo numero: ");
+            string segundoNumeroString = Console.ReadLine();
+            int segundoNumero = Convert.ToInt32(segundoNumeroString);
+
+            int resultado = primeiroNumero + segundoNumero;
+
+            Console.WriteLine("Resultado: " + resultado);
+
+            Console.ReadLine();
+        }
+    }
+}
