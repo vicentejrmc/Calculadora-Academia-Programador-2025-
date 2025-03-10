3e441b9661a3a7262ec223520179d848bd521586
vicentejrmc <vicente_pc@outlook.com>
2025-03-10 14:29:43 -0300

    Adicionada funcionalidade de execução em loop

--- Calculadora.ConsoleApp/Program.cs
+++ Calculadora.ConsoleApp/Program.cs
@@ -4,45 +4,64 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("__________________________");
-            Console.WriteLine("Calculadora Tabajara 2025");
-            Console.WriteLine("__________________________");
+            // bool representa um bit podedendo ser 0 ou 1, apenas um valor 
 
-            Console.WriteLine(" 1 - Somar");
-            Console.WriteLine(" 2 - Subtrair");
-            Console.WriteLine(" S - Sair");
-
-            Console.WriteLine();
-            Console.Write("Escolha uma opção: ");
-            string opcao = Console.ReadLine().ToUpper(); // Converte para Maiúsculo         
-            
-            if(opcao == "S")
+            while (true) //true == true dessa forma sempre será verdadeiro e só será quebrado o Loop quado
+                // a condiçãode break; for executada. 
             {
-                return;
-            }
+                Console.Clear();
+                Console.WriteLine("--------------------------");
+                Console.WriteLine("Calculadora Tabajara 2025");
+                Console.WriteLine("---------------------------");
 
-            Console.Write("Digite o primeiro numero: ");
-            string primeiroNumeroString = Console.ReadLine();
-            int primeiroNumero = Convert.ToInt32(primeiroNumeroString); 
-                
-            Console.Write("Digite o segundo numero: ");
-            string segundoNumeroString = Console.ReadLine();
-            int segundoNumero = Convert.ToInt32(segundoNumeroString);
-            
-            int resultado = 0;
+                Console.WriteLine(" 1 - Somar");
+                Console.WriteLine(" 2 - Subtrair");
+                Console.WriteLine(" S - Sair");
 
-            if (opcao == "1")
-            {
-                resultado = primeiroNumero + segundoNumero;
-            }
-            else
-            {
-                resultado = primeiroNumero - segundoNumero;
-            }
+                Console.WriteLine();
+                Console.Write("Escolha uma opção: ");
+                string opcao = Console.ReadLine().ToUpper(); // Converte para Maiúsculo         
 
-            Console.WriteLine("Resultado: " + resultado);
+                if (opcao == "S")
+                {
+                    break;
+                }
 
-            Console.ReadLine();
+                Console.Write("Digite o primeiro numero: ");
+                string primeiroNumeroString = Console.ReadLine();
+                int primeiroNumero = Convert.ToInt32(primeiroNumeroString);
+
+                Console.Write("Digite o segundo numero: ");
+                int segundoNumero = Convert.ToInt32(Console.ReadLine());
+
+                int resultado = 0;
+
+                if (opcao == "1")
+                {
+                    resultado = primeiroNumero + segundoNumero;
+                }
+                else
+                {
+                    resultado = primeiroNumero - segundoNumero;
+                }
+
+                Console.WriteLine("--------------------------");
+                Console.WriteLine("Resultado = " + resultado);
+                Console.WriteLine("---------------------------");
+
+                Console.Write("Deseja continuar? (S/N): ");
+                string opcaoContinuar = Console.ReadLine().ToUpper();
+
+                if(opcaoContinuar != "S")
+                {
+                    break;
+                }
+            }   
+
+            // break; Também pode ser usado ao final de um laço de repetição while para quebrar o Loop
+            // return; quebra o programa inteiro retornando para o Main, o break; quebra apenas o loop
         }
+
+  
     }
 }
